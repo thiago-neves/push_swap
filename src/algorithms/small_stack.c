0422307579a52dd5c7298045672e9eb1b8b8ed68
thiago-neves <tneves@student.42sp.org.br>
2022-02-01 20:35:55 +0100

    5 random numbers added

--- src/algorithms/small_stack.c
+++ src/algorithms/small_stack.c
@@ -34,11 +34,60 @@ void sort_three_numbers(t_stack *stack, t_array *a)
 	}
 }
 
+void sort_four_numbers(t_stack *stack, t_array *a)
+{
+	push_b(stack);
+	if (!is_sorted(a))
+		sort_three_numbers(stack, &stack->a);
+	push_a(stack);
+	if (a->index[0] > a->index[1] && a->index[0] < a->index[2])
+		swap_a(stack);
+	else if (a->index[0] > a->index[2] && a->index[0] < a->index[3])
+	{
+		swap_a(stack);
+		rotate_a(stack);
+		swap_a(stack);
+		reverse_rotate_a(stack);
+	}
+	else if (a->index[0] > a->index[3])
+		rotate_a(stack);
+}
+
+void sort_five_numbers(t_stack *stack, t_array *a)
+{
+	push_b(stack);
+	if (!is_sorted(a))
+		sort_four_numbers(stack, &stack->a);
+	push_a(stack);
+	if (a->index[0] > a->index[1] && a->index[0] < a->index[2])
+		swap_a(stack);
+	else if (a->index[0] > a->index[2] && a->index[0] < a->index[3])
+	{
+		swap_a(stack);
+		rotate_a(stack);
+		swap_a(stack);
+		reverse_rotate_a(stack);
+	}
+	else if (a->index[0] > a->index[3] && a->index[0] < a->index[4])
+	{
+		reverse_rotate_a(stack);
+		swap_a(stack);
+		rotate_a(stack);
+		rotate_a(stack);
+	}
+	else if (a->index[0] > a->index[4])
+		rotate_a(stack);
+}
+
 int small_stack(t_stack *stack)
 {
-	if (stack->a.length == 2 && !is_sorted(&stack->a))
+	if (stack->a.length == 2)
 		swap_a(stack);
-	else if (stack->a.length == 3 && !is_sorted(&stack->a))
+	else if (stack->a.length == 3)
 		sort_three_numbers(stack, &stack->a);
+	else if (stack->a.length == 4)
+		sort_four_numbers(stack, &stack->a);
+	else if (stack->a.length == 5)
+		sort_five_numbers(stack, &stack->a);
 	return (0);
 }
