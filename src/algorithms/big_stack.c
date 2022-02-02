dcb5109c995c9dfe01cc8cd143137d28601b9f1a
thiago-neves <tneves@student.42sp.org.br>
2022-02-02 19:25:38 +0100

    sort big stack structure added

--- src/algorithms/big_stack.c
+++ src/algorithms/big_stack.c
@@ -1,6 +1,30 @@
 #include "../../inc/push_swap.h"
 
-void big_stack(t_stack *stack)
+void sort_chunk(t_stack *stacks, int last_chunk, int chunk)
 {
-	printf("Big stack: %d\n", stack->length);
+	(void)stacks;
+	(void)last_chunk;
+
+
+	printf("%d\n", chunk);
+}
+
+void sort_big_stack(t_stack *stacks, int chunk_number)
+{
+	int i;
+	int chunk;
+
+	i = 0;
+	chunk = stacks->a.length / chunk_number;
+	while (stacks->a.length > chunk * i)
+	{
+		sort_chunk(stacks, chunk * (i - 1), chunk * i);
+		i++;
+	}
+}
+
+void big_stack(t_stack *stacks)
+{
+	sort_big_stack(stacks, 5);
+	printf("Big stack: %d\n", stacks->length);
 }
